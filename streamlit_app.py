b668b09b19a08a65a537b9e9f37bf8b262a2a664
Vanshaj Bhardwaj <73098377+justvanshaj@users.noreply.github.com>
2024-09-17 18:11:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ hide_github_icon = """
 #GithubIcon {
   visibility: hidden;
 }
-"""
+
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Load the image (replace 'image.jpg' with your file path)
