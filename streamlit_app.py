88e45f8fd50750dcdcbccad9239f1dab678e8c36
Vanshaj Bhardwaj <73098377+justvanshaj@users.noreply.github.com>
2024-10-17 13:00:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 img = Image.open('Banner.png')
 
 # Display the image
-st.image(img, caption='', use_column_width=True)
+st.image(img, caption='', width=100)
 
 # Input for sample
 x = st.number_input("Enter Sample:", min_value=1, step=1)
