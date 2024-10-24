713a3bc4b859f2c5389cfc91fe613052d2a74d96
Vanshaj Bhardwaj <73098377+justvanshaj@users.noreply.github.com>
2024-10-24 13:20:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # Load images for the welcome screen icons
 banner_img = Image.open('Banner2.png')
 sieve_icon = Image.open('sieve_icon.png')  # Placeholder for Sieve Shaker icon
-dal_icon = Image.open('dal_icon.ico')      # Placeholder for Dal Split icon
+dal_icon = Image.open('daal_icon.png')      # Placeholder for Dal Split icon
 
 # trial starts
 # Define session state to manage navigation between screens
